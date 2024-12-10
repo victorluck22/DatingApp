a234e16f16aa1014d86da7a177283ac180460ab2
Victor Chronium <victor@chronium.com.br>
2024-12-10 14:58:08 -0300

    first commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<AppUser> Users { get; set; }
+}
