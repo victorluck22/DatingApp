a81ac8247b7dcac9289a11b134929f0606ebde83
Victor Chronium <victor@chronium.com.br>
2025-01-20 11:13:18 -0300

    frontend adjusts

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,6 +8,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ]
 };
